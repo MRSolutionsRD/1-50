39411bf5a7223a43aa227b43b57ddacb7da12289
MRSolutionsRD <61673292+MRSolutionsRD@users.noreply.github.com>
2020-03-06 13:16:47 -0400

    Add files via upload

---
+++ 1-50/1-50/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Práctica C# 4");
+
+            for (int s = 1; s <= 50; s++) 
+            Console.WriteLine("La secuencia es: " + s);
+
+        }
+    }
+}
